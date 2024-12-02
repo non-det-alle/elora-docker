cd833125b3cbfe2ef9250aac88790c5f0980ad66
Alessandro Aimi <alleaimi95@gmail.com>
2024-12-02 19:44:47 +0100

    start.py: improve readability and propagate SIGTERM

--- build/start.py
+++ build/start.py
@@ -1,31 +1,40 @@
 #! /usr/bin/env python3
 
-import toml
-import os
-import subprocess
+import toml, os, subprocess, signal
 
-# Load configurations
-configpath = os.environ['HOME'] + '/configuration.toml'
-configs = toml.load(configpath)
+# Load configuration
+config = toml.load(os.environ["HOME"] + "/configuration.toml")
 
-# Define iptables rules
-dest = configs['destAddr']
+# Create iptables rules
+dest = config["destAddr"]
 if dest == "127.0.0.1" or dest == "localhost":
+    # docker bridge network gateway
     dest = "172.17.0.1"
-tap = configs['tap']
-subprocess.run(['iptables', '-t', 'nat', '-A', 'PREROUTING', '-i', tap,
-                '-p', 'udp', '-j', 'DNAT', '--to-destination', dest])
-subprocess.run(['iptables', '-t', 'nat', '-A', 'POSTROUTING', '-o', 'eth0',
-                '-p', 'udp', '-j', 'MASQUERADE', '--to-ports', '40000-50000'])
+tap = config["tap"]
+subprocess.run(
+    ["iptables", "-t", "nat", "-A", "PREROUTING", "-i", tap, "-p", "udp"]
+    + ["-j", "DNAT", "--to-destination", dest]
+)
+subprocess.run(
+    ["iptables", "-t", "nat", "-A", "POSTROUTING", "-o", "eth0", "-p", "udp"]
+    + ["-j", "MASQUERADE", "--to-ports", "40000-50000"]
+)
 
 # Run ns-3 simulation
-run = configs['run']
-ns3 = os.environ['NS3DIR'] + '/./ns3'
-target = run['target']
-options = ['--cwd', os.environ['OUTPUT']]
-args = ['--']
-if 'args' in run:
-    for p, v in run['args'].items():
-        args.append('--' + p + '=' + str(int(v) if isinstance(v, bool) else v))
-subprocess.run([ns3, 'run', target] + options + args,
-               stdin=subprocess.PIPE)
+ns3_run = [os.environ["NS3DIR"] + "/./ns3", "run"]
+options = ["--cwd", os.environ["OUTPUT"]]
+target = [config["run"]["target"]]
+if "args" in config["run"] and config["run"]["args"]:
+    target.append("--")
+    for p, v in config["run"]["args"].items():
+        if isinstance(v, bool):
+            v = int(v)
+        target.append("--" + p + "=" + str(v))
+with subprocess.Popen(ns3_run + options + target) as proc:
+
+    def handle(signum, _):
+        proc.send_signal(signum)
+        proc.wait()
+
+    signal.signal(signal.SIGTERM, handle)
+    proc.wait()
